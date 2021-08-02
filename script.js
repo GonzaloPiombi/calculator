9f0d166cdd38ed00803a7973dffbf074cc69c919
Gonzalo <gonzalopiombi@gmail.com>
2021-08-02 13:05:56 -0300

    Add way of making operations after entering the values, operator and pressing the equals button.

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const numberButtons = document.querySelectorAll('.number-button');
+const operatorButtons = document.querySelectorAll('.operator-button');
+const equalsButton = document.querySelector('#equals');
+const clearButton = document.querySelector('#clear-button');
+const display = document.querySelector('.display');
+const smallDisplay = document.querySelector('.small-display');
+
+let num1 = null;
+let num2 = null;
+let result = null;
+let operator = null;
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        pressedButton = e.target.textContent;
+        display.textContent += pressedButton;
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        decideValues();
+        operator = e.target.textContent;
+        display.textContent = '';
+        smallDisplay.textContent = num1 + ' ' + operator;
+    });
+});
+
+equalsButton.addEventListener('click', () => {
+    num2 = display.textContent;
+    result = operate(operator, num1, num2);
+    smallDisplay.textContent = num1 + ' ' + operator + ' ' + num2;
+    display.textContent = result;
+});
+
+function decideValues() {
+    if (num1 === null) {
+        num1 = display.textContent;
+    } else {
+        num2 = display.textContent;
+    }
+}
+
 function add(num1, num2) {
     return (+num1 + +num2);
 }
