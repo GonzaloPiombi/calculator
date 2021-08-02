d41294f591c9175083fcacf5f70d1385a982d25b
Gonzalo <gonzalopiombi@gmail.com>
2021-08-02 12:45:36 -0300

    Add basic operations

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(num1, num2) {
+    return (+num1 + +num2);
+}
+
+function subtract(num1, num2) {
+    return (+num1 - +num2);
+}
+
+function multiply(num1, num2) {
+    return (+num1 * +num2);
+}
+
+function divide(num1, num2) {
+    if (num2 == 0) {
+        return '∞ Don\'t divide by 0 ∞'
+    } else {
+        return (+num1 / +num2);
+    }
+}
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2)
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case 'X':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+    }
+}
